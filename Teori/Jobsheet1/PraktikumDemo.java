48128f19a40f8320a0d13a4d817c60b2a73486fc
galihmaulamumtaz-a11y <galihmaulamumtaz@gmail.com>
2026-08-29 13:51:38 +0700

    JS 1 - PrakTeori - MainDemo

--- Teori/Jobsheet1/PraktikumDemo.java
+++ Teori/Jobsheet1/PraktikumDemo.java
@@ -1,12 +1,12 @@
 public class PraktikumDemo {
     public static void main(String[] args) {
-        System.out.println("=== Objek 1: Kendaraan (class umum/induk) ===");
+        System.out.println("Objek 1: Kendaraan (class umum/induk)");
         Kendaraan kendaraan1 = new Kendaraan();
         kendaraan1.setMerk("Generic Vehicle");
         kendaraan1.tambahKecepatan(80);
         kendaraan1.printInfo();
 
-        System.out.println("\n=== Objek 2: Mobil (turunan Kendaraan) ===");
+        System.out.println("\nObjek 2: Mobil (turunan Kendaraan)");
         Mobil mobil1 = new Mobil();
         mobil1.setMerk("Toyota Avanza");
         mobil1.tambahKecepatan(100);
@@ -14,7 +14,7 @@ public class PraktikumDemo {
         mobil1.setJenisBahanBakar("Bensin");
         mobil1.printInfo();
 
-        System.out.println("\n=== Objek 3: Motor (turunan Kendaraan) ===");
+        System.out.println("\nObjek 3: Motor (turunan Kendaraan)");
         Motor motor1 = new Motor();
         motor1.setMerk("Honda Vario");
         motor1.tambahKecepatan(60);
@@ -22,13 +22,13 @@ public class PraktikumDemo {
         motor1.setTipeMotor("Matic");
         motor1.printInfo();
 
-        System.out.println("\n=== Objek 4: Handphone (berdiri sendiri) ===");
+        System.out.println("\nObjek 4: Handphone (berdiri sendiri)");
         Handphone hp1 = new Handphone();
         hp1.setMerk("Samsung Galaxy A54");
         hp1.isiDaya(45);
         hp1.printInfo();
 
-        System.out.println("\n=== Objek 5: Tas Ransel (berdiri sendiri) ===");
+        System.out.println("\nObjek 5: Tas Ransel (berdiri sendiri)");
         TasRansel tas1 = new TasRansel();
         tas1.setMerk("Eiger");
         tas1.tambahBarang(6);
